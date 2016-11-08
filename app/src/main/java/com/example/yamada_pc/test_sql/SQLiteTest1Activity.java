23af84ba20d263ab0485c10bc27427429f9df8a0
t-YAMADA <16043065@mmm.muroran-it.ac.jp>
2016-11-08 14:23:41 +0900

    rename file

--- app/src/main/java/com/example/yamada_pc/test_sql/SQLiteTest1Activity.java
+++ app/src/main/java/com/example/yamada_pc/test_sql/SQLiteTest1Activity.java
@@ -3,7 +3,7 @@ package com.example.yamada_pc.test_sql;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SQLiteTest1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
